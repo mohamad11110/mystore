ad4c56ce92db713b7d49f604cf2e984f13c405f8
mohamad11110 <149915946+mohamad11110@users.noreply.github.com>
2024-01-15 18:01:29 +0200

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php
+
+$connect = new mysqli("localhost","root","","my_store");
+
+if($connect){
+	 
+}else{
+	echo "Connection Failed";
+	exit();
+	
+}
---
+++ my_store.sql
@@ -0,0 +1,105 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 15, 2024 at 04:57 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `my_store`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_item`
+--
+
+CREATE TABLE `tb_item` (
+  `id` int(11) NOT NULL,
+  `item_code` text NOT NULL,
+  `item_name` text NOT NULL,
+  `price` int(11) NOT NULL,
+  `stock` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tb_item`
+--
+
+INSERT INTO `tb_item` (`id`, `item_code`, `item_name`, `price`, `stock`) VALUES
+(1, 'S4DDddd', 'Monitor LCD', 1000000, 222),
+(3, 'G567', 'Speaker Boom', 1500000, 5),
+(4, 'L009', 'CPU FAN', 600000, 900),
+(6, 'M366', 'Mouse Gaming', 250000, 500);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` text NOT NULL,
+  `password` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`) VALUES
+(1, 'mohamad', 12345),
+(2, 'ahmad', 112233),
+(4, 'hasan', 1122331),
+(5, 'mhmd', 12343);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_item`
+--
+ALTER TABLE `tb_item`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_item`
+--
+ALTER TABLE `tb_item`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
